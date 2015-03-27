63c058737ef7ccb438812725daae43d39cf3604e
sallytheo <sasymonds@gmail.com>
2015-03-26 18:39:32 -0700

    Create Program.java

---
+++ Program.java
@@ -0,0 +1,16 @@
+/*
+ * Manage Recipe Program, ITC 115 Assign 6
+ * 
+ * Main method, only.
+ * 
+ * -Sarah Symonds 3-9-15
+ */
+
+public class Program {
+
+	public static void main(String[] args) {
+		RecipeForm rf = new RecipeForm();
+
+	}
+
+}
